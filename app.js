665499592780ad76173e8c1254462ab8eac86ece
Mich <design@stockmarketcollege.co.za>
2022-04-24 23:56:32 +0200

    commit

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const { Telegraf } = require('telegraf')
-const bot = new Telegraf('5358106715:AAExRwiK2zifENOEg_UPbCLH-ZWERHYeA8A')
+const TelegramBot = require('node-telegram-bot-api')
+const token = '5319740178:AAGjmBJSOewm_6AXiAkkaV_pHACL0qKrBwY'
+const bot = new TelegramBot(token, {
+    polling: true
+});
+
+bot.on('message', function(msg) {
+    console.log('Received Message',msg);
+    const res = `Hi ${msg.from.first_name}..... Message received`;
+    bot.sendMessage(msg.chat.id,res);
+});
+
+bot.onText(/\/echo (.+)/,function(msg,match) {
+    console.log('received echo request');
+    console.log(match);
+    const res= 'received echo';
+    bot.sendMessage(msg.chat.id,res);
+});
+
 
